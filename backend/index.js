8e6e5a21f34a4ccb2ce9f935b8a166475d68defe
Subhasis <iamsubhasis2003@gmail.com>
2025-05-24 13:00:54 +0530

    setup project files

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,11 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
 
-const app  = express()
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5050;
 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
